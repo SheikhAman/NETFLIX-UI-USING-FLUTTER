216345507324af5d9b64945a96f142e9b990baf7
ShekAdnan <adnansk418@gmail.com>
2021-11-18 23:05:59 +0600

    appbar

---
+++ myapp/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Image(
+          image: AssetImage('assets/images/netflix_logo.png'),
+        ),
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 30.0),
+          onPressed: () {
+            print('Menu');
+          },
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.black,
+        ),
+        actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.only(right: 30.0),
+            onPressed: () => print('Search'),
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
